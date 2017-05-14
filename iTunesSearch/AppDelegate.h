0710492d16c167b1688d7157e3ff46b7d513087e
Stephen Lindauer <0xStephen@protonmail.com>
2017-05-14 11:55:02 -0700

    Initial Commit

---
+++ iTunesSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTunesSearch
+//
+//  Created by Stephen Lindauer on 5/14/17.
+//  Copyright © 2017 Stephen Lindauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTunesSearch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iTunesSearch
+//
+//  Created by Stephen Lindauer on 5/14/17.
+//  Copyright © 2017 Stephen Lindauer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
